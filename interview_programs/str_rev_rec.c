1f85e01a1e0aaf96584207cfab946cc73ce8a6ba
pranitekatpure <pranitekatpure@gmail.com>
2020-08-27 18:36:59 +0530

    added string reverse using recursion

---
+++ interview_programs/str_rev_rec.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+char* reverse(char *str);
+
+void main()
+{
+	int i, j, k;
+	char str[100], *rev;
+	printf("Enter the string:\t");
+	scanf("%s", str);
+	printf("\nThe original string is: %s\n", str);
+	rev = reverse(str);
+	printf("The reversed string is: %s\n", rev);
+	getch();
+}
+
+char* reverse(char *str)
+{
+	static int i = 0;
+	static char rev[100];
+	if(*str)
+	{
+		reverse(str+1);
+		rev[i++] = *str;
+	}
+	return rev;
+}
